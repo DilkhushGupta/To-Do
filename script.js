229548f0d9f04ba612ff1862edc485c7c24a1ee7
DilkhushGupta <guptadilkhush3@gmail.com>
2024-05-26 11:04:06 +0530

    add delete icon

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function addTask(){
          alert("Please Enter Task then Add.");
     else{
         let box = document.createElement("div");
-        box.innerHTML = `${inputText.value} <button>del</button>`;
+        box.innerHTML = `${inputText.value} <i class="fa-solid fa-trash"></i>`;
         listContainer.appendChild(box);
         inputText.value = "";
 
-        let del_btn = box.querySelector("button");
+        let del_btn = box.querySelector("i");
         del_btn.addEventListener("click",remove);
         function remove(){
             box.remove();
